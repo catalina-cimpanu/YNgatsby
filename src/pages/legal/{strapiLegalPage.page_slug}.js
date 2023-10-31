51ab908ac0d1bca0f0a57953b48ddf6cce883ba0
catalina.cimpanu <catalina.cimpanu@gmail.com>
2023-10-31 12:41:06 +0100

    programatically generate legal pages with data from strapi

---
+++ src/pages/legal/{strapiLegalPage.page_slug}.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { graphql } from "gatsby";
+import Layout from "../../../src/components/Layout";
+import * as legalStyles from "./legal.module.css";
+
+const LegalPageTemplate = ({ data }) => {
+  const {
+    strapiLegalPage: {
+      page_title,
+      updatedAt,
+      page_content: {
+        data: {
+          childMarkdownRemark: { html },
+        },
+      },
+    },
+  } = data;
+  return (
+    <Layout nocontents>
+      <div className={legalStyles.container}>
+        <h1>{page_title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: html }} />
+        <p>This document was last updated on {updatedAt}</p>
+      </div>
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  query ($page_slug: String) {
+    strapiLegalPage(page_slug: { eq: $page_slug }) {
+      page_title
+      updatedAt(formatString: "DD MMMM YYYY")
+      page_content {
+        data {
+          childMarkdownRemark {
+            html
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default LegalPageTemplate;
