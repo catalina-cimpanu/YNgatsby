723535528d2eb227edd3308d6be4b1bbff0e6905
catalina.cimpanu <catalina.cimpanu@gmail.com>
2023-11-02 13:06:37 +0100

    added 404 page

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,7 +52,7 @@ const strapiConfig = {
 module.exports = {
   siteMetadata: {
     title: `Young Neuros`,
-    siteUrl: `https://youngneuros.netlify.app/`,
+    siteUrl: `http://localhost:8000/`,
     description: "Stay smart. Stay in touch.",
     twitterUsername: `@Young_Neuros`,
     image: "hero_image.png",
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,42 +1,38 @@
 import * as React from "react";
 import { Link } from "gatsby";
+import styled from "styled-components";
 import Layout from "../components/Layout";
 import SEO from "../components/SEO";
-
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-};
-
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-};
+import TitleH1 from "../components/titles/TitleH1";
+import NotFoundImg from "../images/not_found.inline.svg";
 
 const NotFoundPage = () => {
   return (
     <Layout>
-      <h1>Page not found</h1>
-      <p>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
+      <TitleH1 title="Page not found" />
+      <Wrapper>
+        <NotFoundImg className="img" />
+        <p>
+          Sorry, we couldn’t find what you were looking for.
+          <br />
+          <br />
+          <Link to="/">Go Home</Link>
+        </p>
+      </Wrapper>
     </Layout>
   );
 };
+
+const Wrapper = styled.div`
+  display: grid;
+  place-items: center;
+  text-align: center;
+  margin-bottom: 3rem;
+  .img {
+    width: 80%;
+    margin: 3rem;
+  }
+`;
 
 export default NotFoundPage;
 
@@ -46,8 +42,8 @@ export const Head = () => {
       title="Page not found"
       description="The page that you are looking for doesn't exist."
       pathname="/404"
-      // image={publicURL}
-      // alt={alternativeText}
+      image="not_found.png"
+      alt="not found image"
     />
   );
 };
